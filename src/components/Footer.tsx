463b0d7e15431d56cdaed68e012796fa131b5eaf
Nicu-ro <nicusor.nicu2008@gmail.com>
2026-04-21 11:37:26 +0300

    Changes for comp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,22 +5,22 @@ export default function Footer() {
 
     return (
         <footer className="site-footer footer-layout">
-            {/* Left Side: School Info (This will shrink on mobile) */}
+         
             <div className="info-text footer-info-left">
                 <span>Liceul "Traian Vuia" Craiova</span>
                 <span>Clasa: XI-C</span>
                 <span>Specializarea: Matematica-Informatica</span>
-                <span>Proiect realizat de Galbinu Nicusor</span>
+                <span>Profesori coordonatori: Neacsu Claudia , Toma Denisa</span>
             </div>
 
-            {/* Middle Side: Copyright (This stays centered) */}
+           
             <div className="footer-center">
                 <Link href="https://www.traianvuia.ro/">
                     © {year} Liceul Traian Vuia
                 </Link>
             </div>
 
-            {/* Right Side: Empty spacer to keep the middle balanced */}
+        
             <div className="footer-spacer"></div>
         </footer>
     );
